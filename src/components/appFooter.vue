ba0a9bbee7bdb5fdefbb4a271a78c16cac0c199b
YAAP <yaapis@gmail.com>
2017-08-16 15:53:48 +0300

    typos

--- src/components/appFooter.vue
+++ src/components/appFooter.vue
@@ -19,9 +19,6 @@
       <li>
         <router-link :to="{ name: 'login'}">Login</router-link>
       </li>
-      <li>
-        <router-link :to="{ name: 'login'}">Login</router-link>
-      </li>
     </ul>
   </footer>
 
